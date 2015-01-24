7ee78193ceefcf4a55666b675c5e98536d39d256
Jeremy Bird <fireninja237@gmail.com>
2015-01-24 10:05:59 -0500

    Box additions
    And fiddling with some of the physics.

---
+++ Assets/Scripts/BounceAction.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BounceAction : MonoBehaviour {
+	public float bounceFactor = 100.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void onTrigger (Collision other) {
+		if(other.gameObject.tag=="Player") {
+			other.gameObject.rigidbody2D.AddForce(new Vector2(0.0f, bounceFactor));
+			Debug.Log("BOUNCE!");
+		}
+	}
+}
