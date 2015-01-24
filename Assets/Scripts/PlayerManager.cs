6ba895e357fa46ee65a72f94f19bbf0454df6ae4
Chrs2021 <chrs2021@gmail.com>
2015-01-24 16:39:24 -0500

    test changes
    made hitbox detection to player
    changed collision type to black and yellow test objects

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -13,36 +13,104 @@ public enum PlayerAction{
 }
 
 public class PlayerManager : MonoBehaviour {
-
-	public static int hitPoints;
-	public static PlayerAction currentState;
+	
+	public int hitPoints;
+	public PlayerAction currentState;
 	public bool canFly = false;
 	public float flightSpeed = 0.25f;
-	public float MovementSpeed = 0.8f;
-	Vector3 oldPos;
-	Vector3 newPos;
+	public float MovementSpeed = 1600.0f;
+	public bool isLookingRight;
+	public SpriteRenderer playerSprite;
+	public float jumpForce;
+	public float maxVelo;
+	//debugging vars 
+	public float yVelo;
+	public float xVelo;
+	public float inputH;
+	public float inputV;
+	
 	// Use this for initialization
 	void Start () {
 		hitPoints = 3;
+		isLookingRight=true;
+	//	playerSprite = gameObject.GetComponent<SpriteRenderer>();
+		maxVelo = 2300;
+		jumpForce = 250.0f;
 	}
-	
+
+	void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.tag == "Deathbox")
+			hitPoints--;
+		if(other.gameObject.tag == "Enemy")
+			hitPoints--;
+		// this insures that the player isn't stopped by the NPC control blocks
+		if (other.gameObject.tag == "MotionControlBlock")
+			Physics2D.IgnoreCollision(this.collider2D, other.collider);
+		
+	}
+
 	// Update is called once per frame
 	void Update () {
 	}
-
+	//Physics updating
 	void FixedUpdate() {
 		float movementHorizontal = Input.GetAxis ("Horizontal");
+		float movementVertical = Input.GetAxis ("Vertical");
 		float adjustedSpeed = MovementSpeed;
-		if (rigidbody2D.velocity.y != 0.0f)
-			if (canFly)
-				adjustedSpeed *= flightSpeed;
-		else 
-			adjustedSpeed = 0.0f;
-		rigidbody2D.AddForce (new Vector3 (movementHorizontal * adjustedSpeed, 0));
-
+		
+		//begin debug code
+		yVelo = rigidbody2D.velocity.y; 
+		xVelo = rigidbody2D.velocity.x; 
+		inputH = movementHorizontal;
+		inputV = movementVertical;
+		//end debug code
+		if (movementHorizontal < 0 || movementHorizontal > 0 ) {
+			currentState = PlayerAction.Walking;		
+		} else {
+			if(rigidbody2D.velocity.y != 0.0f )
+				currentState = PlayerAction.Jump;
+			else
+				currentState = PlayerAction.Standing;
+		}
+		if(rigidbody2D.velocity.y == 0.0f){
+			if (rigidbody2D.velocity.y != 0.0f)
+				if (canFly)
+					adjustedSpeed *= flightSpeed;
+			else 
+				adjustedSpeed = 0.0f;
+			if(Mathf.Abs(rigidbody2D.velocity.x) < maxVelo)
+			{
+				if (movementHorizontal > 0) {
+					isLookingRight = true;
+					rigidbody2D.AddForce (new Vector2 (1 * adjustedSpeed * Time.deltaTime, 0));
+				}else if(movementHorizontal < 0){
+					isLookingRight = false;
+					rigidbody2D.AddForce (new Vector2 (-1 * adjustedSpeed * Time.deltaTime, 0));
+				}
+			}else if(Mathf.Abs(rigidbody2D.velocity.x) > maxVelo)
+			{
+				if(isLookingRight)
+					rigidbody2D.velocity = new Vector2(1 * maxVelo,0);
+				else
+					rigidbody2D.velocity = new Vector2(-1 * maxVelo,0);
+				
+			}
+		}
+		
+		
+		//jumping
+		if(movementVertical >0)
+		{
+			if(currentState != PlayerAction.Jump && currentState != PlayerAction.Walking){
+				currentState = PlayerAction.Jump;
+				rigidbody2D.AddForce(new Vector2(0, jumpForce));
+			}
+		}else if(movementVertical <0){
+			currentState = PlayerAction.Duck;
+		}
 	}
 	void getInput()
 	{
-
+		
 	}
 }
