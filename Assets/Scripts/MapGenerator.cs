27d059d6b40dd971a5377a47daa0d4dc9a1272d5
Jeremy Bird <fireninja237@gmail.com>
2015-01-23 22:48:37 -0500

    Added ground, stock player motion
    Added two new ground types, generated simple map with modifiable X and
    Y, gave basic motion control to player

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,10 +4,18 @@ using System.Collections;
 public class MapGenerator : MonoBehaviour {
 
 	public GameObject blackCube;
+	public GameObject player;
+	public float groundWidth, groundHeight;
 
 	// Use this for initialization
 	void Start () {
-	
+		float i,j;
+
+		for (i=0.0f; i<groundWidth; i++) {
+			for (j=0.0f; j<groundHeight; j++) {
+				Instantiate (blackCube, new Vector3 (i,0.0f-j), new Quaternion(0.0f,0.0f,0.0f,0.0f));
+			}
+		}
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/Motion.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Motion : MonoBehaviour {
+
+	public float speed;
+	public bool canFly = false;
+	public float flightSpeed = 0.25f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate() {
+		float movementHorizontal = Input.GetAxis ("Horizontal");
+		float adjustedSpeed = speed;
+		if (rigidbody2D.velocity.y != 0.0f)
+				if (canFly)
+						adjustedSpeed *= flightSpeed;
+				else 
+						adjustedSpeed = 0.0f;
+		rigidbody2D.AddForce (new Vector3 (movementHorizontal * speed, 0));
+	}
+}
