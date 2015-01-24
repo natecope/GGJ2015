876df703410e509b58b55bf6e0e78a99093d193f
Chrs2021 <chrs2021@gmail.com>
2015-01-24 12:16:35 -0500

    near-complete player manager
    added max velocity
    added debugging properties
    added states

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -14,32 +14,82 @@ public enum PlayerAction{
 
 public class PlayerManager : MonoBehaviour {
 
-	public static int hitPoints;
-	public static PlayerAction currentState;
+	public int hitPoints;
+	public PlayerAction currentState;
 	public bool canFly = false;
 	public float flightSpeed = 0.25f;
-	public float MovementSpeed = 0.8f;
-	Vector3 oldPos;
-	Vector3 newPos;
+	public float MovementSpeed = 1000.0f;
+	public bool isLookingRight;
+	public SpriteRenderer playerSprite;
+	public float jumpForce;
+	public float maxVelo;
+	//debugging vars 
+	public float yVelo;
+	public float xVelo;
+
 	// Use this for initialization
 	void Start () {
 		hitPoints = 3;
+		isLookingRight=true;
+		playerSprite = gameObject.GetComponent<SpriteRenderer>();
+		maxVelo = 3;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (!isLookingRight)
+			playerSprite.color = new Color32 (241, 20, 10, 255);
+		else
+			playerSprite.color = new Color32 (10, 128, 56, 255);
+		if(currentState == PlayerAction.Duck)
+			playerSprite.color = new Color32 (0, 0, 156, 255);
 	}
-
+	//Physics updating
 	void FixedUpdate() {
 		float movementHorizontal = Input.GetAxis ("Horizontal");
+		float movementVertical = Input.GetAxis ("Vertical");
 		float adjustedSpeed = MovementSpeed;
+
+		//begin debug code
+		yVelo = rigidbody2D.velocity.y; 
+		xVelo = rigidbody2D.velocity.x; 
+		//end debug code
+		if ((movementHorizontal < 0 || movementHorizontal > 0) ) {
+			currentState = PlayerAction.Walking;		
+		} else {
+			if(rigidbody2D.velocity.y != 0.0f )
+				currentState = PlayerAction.Jump;
+			else
+				currentState = PlayerAction.Standing;
+		}
+		if(rigidbody2D.velocity.y == 0.0f && currentState != PlayerAction.Duck){
 		if (rigidbody2D.velocity.y != 0.0f)
 			if (canFly)
 				adjustedSpeed *= flightSpeed;
 		else 
 			adjustedSpeed = 0.0f;
-		rigidbody2D.AddForce (new Vector3 (movementHorizontal * adjustedSpeed, 0));
+		if(Mathf.Abs(rigidbody2D.velocity.x) < maxVelo)
+		{
+			if (movementHorizontal > 0) {
+				isLookingRight = true;
+				rigidbody2D.AddForce (new Vector2 (1 * adjustedSpeed * Time.deltaTime, 0));
+			}else if(movementHorizontal < 0){
+				isLookingRight = false;
+				rigidbody2D.AddForce (new Vector2 (-1 * adjustedSpeed * Time.deltaTime, 0));
+			}
+		}
 
+		}
+		//jumping
+		if(movementVertical >0)
+		{
+			if(currentState != PlayerAction.Jump && currentState != PlayerAction.Walking){
+			currentState = PlayerAction.Jump;
+			rigidbody2D.AddForce(new Vector2(0, jumpForce));
+			}
+		}else if(movementVertical <0){
+			currentState = PlayerAction.Duck;
+		}
 	}
 	void getInput()
 	{
