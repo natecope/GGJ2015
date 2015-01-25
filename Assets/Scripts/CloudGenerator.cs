5ccf0ce4da53aa59d5d30cace5c438143c3ce0c8
Jeremy Bird <fireninja237@gmail.com>
2015-01-25 12:25:46 -0500

    Ground Update
    Changed the filter for the ground from bilinear to point, which looks a
    little more pixelly but removes a problem where Unity was adding lines
    to the sprites.
    
    Also added new Ground prefabs.

---
+++ Assets/Scripts/CloudGenerator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudGenerator : MonoBehaviour {
+
+	public float cloudSpeed;
+	public Sprite[] clouds;
+
+	public int cloudCount = 0;
+
+	// Use this for initialization
+	void Start () {
+		cloudCount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
