a5a4519a9c7c2295a9d44cbb740d6c34e5719021
Chrs2021 <chrs2021@gmail.com>
2015-01-25 13:43:19 -0500

    added chicken shrews
    chicken shrews

---
+++ Assets/Scripts/CameraJumpController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraJumpController : MonoBehaviour {
+
+	public GameObject camera;
+	public GameObject hero;
+
+	private Animator _camAnimator;
+	
+	// Use this for initialization
+	void Start () {
+		_camAnimator = camera.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (hero.transform.rigidbody2D.velocity.y > 0) {
+			_camAnimator.SetBool("shouldGoD",false);
+			_camAnimator.SetBool("shouldGoU",true);
+			//Debug.Log("Jumping Up");
+		} else if (hero.transform.rigidbody2D.velocity.y == 0) {
+			_camAnimator.SetBool("shouldGoU",false);
+			_camAnimator.SetBool("shouldGoD",true);
+			//Debug.Log ("Landed");
+		}
+	}
+}
