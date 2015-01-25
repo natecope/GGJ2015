79bb02031f7b5a705746bee18722361565a8296a
Nathan Cope <natecope@gmail.com>
2015-01-24 22:49:30 -0500

    Audio files and manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+	public AudioClip music;
+
+	public static AudioManager instance;
+
+	private AudioSource[] audioSource;
+	// Use this for initialization
+	void Start () {
+	
+		instance = this;
+		audioSource = GetComponents<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
