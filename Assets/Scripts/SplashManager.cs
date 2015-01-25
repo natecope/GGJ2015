de292f93d996b57cd2ab8ecc689aa63c835753bd
Nathan Cope <natecope@gmail.com>
2015-01-25 14:20:47 -0500

    added splash manager start button

---
+++ Assets/Scripts/SplashManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if((Input.GetKey (KeyCode.W) || (Input.GetKey (KeyCode.Joystick1Button0))) && (Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.Joystick2Button0))){
+
+			Application.LoadLevel(0);
+
+		}
+	}
+}
